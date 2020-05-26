324543c26af08de998405785bcb9b1f1c6ed9659
ttay <ttay@umich.edu>
2020-05-26 16:39:37 +0800

    finished insert? not sure

--- critBits/critbit.fs
+++ critBits/critbit.fs
@@ -14,39 +14,37 @@ type Node =
     | Leaf of key: string
     | Intermediate of NodeDatum
 and
-    [<Struct>]
     NodeDatum =
-    { byte: int32; // the byte that differs
+    { byte: int; // the byte that differs
      otherBits: byte; // a single byte where every bit except the critical bit is true
-     left: Node;
-     right: Node; }
+     mutable left: Node;
+     mutable right: Node; }
+
+open System.Text;
 
 module private Helpers =
     let inline goLeft (otherBits: byte) (c: byte) =
         ((1 + int (otherBits ||| c)) >>> 8) = 0
 
-    open System.Text;
     // finds the differing byte between strings: str and u
     // Recall that in .NET, strings are UTF-16!
     // So, we must cast the string to a byte array, using
     // some sort of decoding logic
     // For simplicity, this library assumes a UTF-8 encoded string
-    // If necessary, you can change the encoding here.
-    let inline findDifferingByte (str: string) (bestMatch: string) =
-        let uBytes = Encoding.UTF8.GetBytes str // u, p to match the paper
-        let pBytes = Encoding.UTF8.GetBytes bestMatch
+    // If necessary, you can change the encoding in the Add() function
+    let inline findDifferingByte (uBytes: byte[]) (pBytes: byte[]): struct (int * byte) voption =
         assert (Array.length pBytes >= Array.length uBytes)
         let rec helper newByte =
             if newByte >= Array.length uBytes
             then
                 if Array.length pBytes = Array.length uBytes
                 then ValueNone // no differing bits; arrays are identical
-                else ValueSome pBytes.[newByte] // next available byte
+                else ValueSome struct (newByte, pBytes.[newByte]) // next available byte
             else
                 let p = pBytes.[newByte]
                 let u = uBytes.[newByte]
                 if p <> u
-                then ValueSome (p ^^^ u) // found differing bit!
+                then ValueSome struct (newByte, (p ^^^ u)) // found differing bit!
                 else helper (newByte + 1) // loop()
         helper 0
 
@@ -58,6 +56,9 @@ module private Helpers =
         if a = 0uy
         then newOtherBits
         else findDifferingBit a
+
+    let inline constructNode byte otherBits left right =
+        Intermediate {byte=byte; otherBits=otherBits; left=left; right=right}
 
 open Helpers
 
@@ -86,6 +87,9 @@ type CritBitTree() =
 
     member val Count = 0 with get, set
 
+    member _.Clear() =
+        root <- None  // ahh... the good thing about having a GC...
+
     member _.Contains(str: string): bool =
         match root with
         | None -> false
@@ -93,21 +97,60 @@ type CritBitTree() =
             let key = walkTree str n
             key = str
 
-    member _.Insert(str: string) =
+    member _.Add(str: string): bool =
         match root with
         | None -> // empty tree
-            root <- Some {byte=0; otherBits=0uy; data= Leaf str;}
-        | Some root ->
-            let bestMatch = walkTree str root
-            match findDifferingByte str bestMatch with
-            | ValueNone -> () // insert has no effect if key exists
-            | ValueSome newByte ->
-                let newOtherBits = findDifferingBit newByte
-                let newOtherBits = newOtherBits ^^^ 255uy
-                let c = bestMatch.[newByte]
-                let direction = goLeft newOtherBits c
-                // allocate new node
-                //let newNode: Node =
-                //    {byte = newByte;
-                //    otherbits = newOtherBits;
-                //    }
+            root <- Some (Leaf str)
+            true  // adding to empty tree succeeds
+        | Some rootNode ->
+            let bestMatch = walkTree str rootNode
+            let uBytes = Encoding.UTF8.GetBytes str
+            let pBytes = Encoding.UTF8.GetBytes bestMatch
+            match findDifferingByte uBytes pBytes with
+            | ValueNone -> false // insert has no effect if key exists
+            | ValueSome (newByte, newOtherBits) ->
+                let newOtherBits = (findDifferingBit newOtherBits) ^^^ 255uy
+                let isStrGoesToRightChild = goLeft newOtherBits pBytes.[newByte]
+                match rootNode with
+                | Leaf _ ->
+                    // unfortunately, need to special case
+                    // the root node leaf case
+                    let newNode =
+                        if isStrGoesToRightChild
+                        then constructNode newByte newOtherBits rootNode (Leaf str)
+                        else constructNode newByte newOtherBits (Leaf str) rootNode
+                    root <- Some newNode
+                    true
+                | Intermediate _ ->
+                let rec updateParentForNewNode (q: NodeDatum): unit =
+                    let c =
+                        if q.byte < Array.length uBytes
+                        then uBytes.[q.byte]
+                        else 0uy
+                    let direction = goLeft q.otherBits c
+                    let nextNode =
+                        if direction then q.left else q.right
+                    match nextNode with
+                    | Leaf _ ->
+                        let newNode =
+                            if isStrGoesToRightChild
+                            then constructNode newByte newOtherBits nextNode (Leaf str)
+                            else constructNode newByte newOtherBits (Leaf str) nextNode
+                        // update parent node
+                        if direction
+                        then q.left <- newNode
+                        else q.right <- newNode
+                    | Intermediate x when
+                        (x.byte > newByte) ||
+                            (x.byte = newByte && x.otherBits > newOtherBits) ->
+                        let newNode =
+                            if isStrGoesToRightChild
+                            then constructNode newByte newOtherBits nextNode (Leaf str)
+                            else constructNode newByte newOtherBits (Leaf str) nextNode
+                        // update parent node
+                        if direction
+                        then q.left <- newNode
+                        else q.right <- newNode
+                    | Intermediate x ->
+                        updateParentForNewNode x
+                true
